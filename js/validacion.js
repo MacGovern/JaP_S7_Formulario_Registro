fcd0b95021bb45711ff7bc9b71f3c7680c571e1d
lg <leonardoagustingimenez@gmail.com>
2023-08-09 10:13:51 -0300

    Agregamos validacion de email

--- js/validacion.js
+++ js/validacion.js
@@ -1,3 +1,9 @@
+const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+function validateEmail(email){
+  return emailPattern.test(email);
+}
+
 function showAlertSuccess() {
     document.getElementById("alert-success").classList.add("show");
     setTimeout(() => {
@@ -17,6 +23,7 @@ document.getElementById("regBtn").addEventListener("click", () => {
     if(document.getElementById("nombre").value != "" &&
        document.getElementById("apellido").value != "" &&
        document.getElementById("email").value != "" &&
+       validateEmail(document.getElementById("email").value) &&
        document.getElementById("password1").value.length >= 6 &&
        document.getElementById("password1").value === document.getElementById("password2").value &&
        document.getElementById("terminos").checked){
