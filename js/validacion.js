a12281deb4bc544510778e61c2cceba11db39e36
lg <leonardoagustingimenez@gmail.com>
2023-08-09 10:51:02 -0300

    Pasamos de tener un igual a dos iguales en los diferentes

--- js/validacion.js
+++ js/validacion.js
@@ -20,9 +20,9 @@ function showAlertError() {
 
 
 document.getElementById("regBtn").addEventListener("click", () => {
-    if(document.getElementById("nombre").value != "" &&
-       document.getElementById("apellido").value != "" &&
-       document.getElementById("email").value != "" &&
+    if(document.getElementById("nombre").value !== "" &&
+       document.getElementById("apellido").value !== "" &&
+       document.getElementById("email").value !== "" &&
        validateEmail(document.getElementById("email").value) &&
        document.getElementById("password1").value.length >= 6 &&
        document.getElementById("password1").value === document.getElementById("password2").value &&
